b0c08be99b1b1309562a00e6cd2f3d88b4488c4a
William Zerega <zerega85@gmail.com>
2018-12-05 19:13:35 -0500

    not finding R

---
+++ app/src/main/java/com/billzerega/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,39 @@
+package com.billzerega.android.hellomoon;
+
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class HelloMoonFragment extends Fragment {
+    private AudioPlayer mPlayer = new AudioPlayer();
+    private Button mPlayButton;
+    private Button mStopButton;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup parent, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon, parent, false);
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.play(getActivity());
+            }
+        });
+
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.stop();
+            }
+        });
+
+        return v;
+    }
+}
